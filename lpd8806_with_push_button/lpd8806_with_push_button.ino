0f5b57af836a561b7bcc07941c7a26a118033768
blake messer <rblakemesser@gmail.com>
2013-08-11 17:01:48 -0500

    first commit

---
+++ lpd8806_with_push_button/lpd8806_with_push_button.ino
@@ -0,0 +1,192 @@
+#include "LPD8806.h"
+#include "SPI.h"
+
+// Example to control LPD8806-based RGB LED Modules in a strip
+
+/*****************************************************************************/
+
+// Sequences
+int numSequences = 5;
+int currentSequence = 0;
+
+// Number of RGB LEDs in strand:
+int nLEDs = 14;
+
+// Chose 2 pins for output; can be any valid output pins:
+int dataPin  = 2;
+int clockPin = 3;
+
+
+// Parameters for Switch
+int switchReader;
+int myButton = 4; // press button in pin D4
+int buttonReading;
+int last;
+
+// First parameter is the number of LEDs in the strand.  The LED strips
+// are 32 LEDs per meter but you can extend or cut the strip.  Next two
+// parameters are SPI data and clock pins:
+LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
+
+// You can optionally use hardware SPI for faster writes, just leave out
+// the data and clock pin parameters.  But this does limit use to very
+// specific pins on the Arduino.  For "classic" Arduinos (Uno, Duemilanove,
+// etc.), data = pin 11, clock = pin 13.  For Arduino Mega, data = pin 51,
+// clock = pin 52.  For 32u4 Breakout Board+ and Teensy, data = pin B2,
+// clock = pin B1.  For Leonardo, this can ONLY be done on the ICSP pins.
+//LPD8806 strip = LPD8806(nLEDs);
+
+void setup() {
+  Serial.begin(9600);
+  // Button
+  pinMode(myButton, INPUT);
+  currentSequence = 0;
+  // Start up the LED strip
+  strip.begin();
+
+  // Update the strip, to start they are all 'off'
+  strip.show();
+}
+
+
+void loop() {
+  buttonReading = digitalRead(myButton);
+  if (buttonReading != last) {
+    if (buttonReading == HIGH) {
+      
+      nextSequence(currentSequence); 
+      currentSequence += 1;
+      if (currentSequence >= numSequences) {
+        currentSequence = 0;
+      }
+
+    }
+    last = buttonReading;
+  }
+  // Send a simple pixel chase in...
+//  colorChase(strip.Color(127, 127, 127), 50); // White
+//  colorChase(strip.Color(127,   0,   0), 50); // Red
+//  colorChase(strip.Color(127, 127,   0), 50); // Yellow
+//  colorChase(strip.Color(  0, 127,   0), 50); // Green
+//  colorChase(strip.Color(  0, 127, 127), 50); // Cyan
+
+
+  // Fill the entire strip with...
+//  colorWipe(strip.Color(127,   0,   0), 50);  // Red
+//  colorWipe(strip.Color(  0, 127,   0), 50);  // Green
+//  colorWipe(strip.Color(  0,   0, 127), 50);  // Blue
+
+  
+}
+int nextSequence(int current) {
+  String logOne = "running sequence: ";
+  String logStatement = logOne + current;
+  Serial.println(logStatement);
+  switch (current) {
+     case 0:
+       rainbow(10);
+       break;
+     case 1:
+       colorChase(strip.Color(  0,   0, 127), 50); // Blue
+       colorChase(strip.Color(127,   0, 127), 50); // Violet
+       break;
+     case 2:
+       rainbowCycle(0);
+       break;
+     case 3:
+       colorWipe(strip.Color(127,   0,   0), 50);  // Red
+       colorWipe(strip.Color(  0, 127,   0), 50);  // Green
+       break;
+     case 4:
+       colorWipe(strip.Color(  0,   0,   127), 50);  // Blue
+       break;
+  }
+}
+
+void rainbow(uint8_t wait) {
+  int i, j;
+   
+  for (j=0; j < 384; j++) {     // 3 cycles of all 384 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel( (i + j) % 384));
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
+// Slightly different, this one makes the rainbow wheel equally distributed 
+// along the chain
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+  
+  for (j=0; j < 384 * 5; j++) {     // 5 cycles of all 384 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 384-color wheel
+      // (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 384 is to make the wheel cycle around
+      strip.setPixelColor(i, Wheel( ((i * 384 / strip.numPixels()) + j) % 384) );
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
+// Fill the dots progressively along the strip.
+void colorWipe(uint32_t c, uint8_t wait) {
+  int i;
+
+  for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+// Chase one dot down the full strip.
+void colorChase(uint32_t c, uint8_t wait) {
+  int i;
+
+  // Start by turning all pixels off:
+  for(i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, 0);
+
+  // Then display one pixel at a time:
+  for(i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c); // Set new pixel 'on'
+    strip.show();              // Refresh LED states
+    strip.setPixelColor(i, 0); // Erase pixel, but don't refresh!
+    delay(wait);
+  }
+
+  strip.show(); // Refresh to turn off last pixel
+}
+
+
+/* Helper functions */
+
+//Input a value 0 to 384 to get a color value.
+//The colours are a transition r - g -b - back to r
+
+uint32_t Wheel(uint16_t WheelPos) {
+  byte r, g, b;
+  switch(WheelPos / 128)
+  {
+    case 0:
+      r = 127 - WheelPos % 128;   //Red down
+      g = WheelPos % 128;      // Green up
+      b = 0;                  //blue off
+      break; 
+    case 1:
+      g = 127 - WheelPos % 128;  //green down
+      b = WheelPos % 128;      //blue up
+      r = 0;                  //red off
+      break; 
+    case 2:
+      b = 127 - WheelPos % 128;  //blue down 
+      r = WheelPos % 128;      //red up
+      g = 0;                  //green off
+      break; 
+  }
+  return(strip.Color(r,g,b));
+}
---
+++ toggle_blink_push_button/toggle_blink_push_button.ino
@@ -0,0 +1,36 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+int myswitch = 2; 
+int lit;
+int reading;
+int last;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(myswitch, INPUT);
+  pinMode(led, OUTPUT);   
+  lit = LOW;  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  reading = digitalRead(myswitch);
+  if (reading != last) {
+    if (reading == HIGH) {
+      Serial.println("CHANGE PLACES");
+      lit = !lit;
+      delay(50);
+      digitalWrite(led, lit);    // turn the LED off by making the voltage LOW
+    }
+    last = reading;
+  }
+}
